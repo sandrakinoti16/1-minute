629082ea9145c63acf8cd44376bd8d194e80b497
sandra kinoti <sandra.kinoti@student.moringaschool.com>
2022-05-11 23:43:07 +0300

    save

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
+import dbm
 from flask import Flask 
 from config import config_options
 from flask_mail import Mail
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_uploads import IMAGES, UploadSet,configure_uploads
 
 db = SQLAlchemy()
@@ -13,21 +15,33 @@ login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 photos = UploadSet('photos',IMAGES)
+migrate = Migrate()
+
+
 
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_options[config_name])
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= True
+
     from .auth import auth as authentication_blueprint
     from .main import main as main_blueprint
 
     app.register_blueprint(authentication_blueprint)
     app.register_blueprint(main_blueprint)
 
+
+
+
     login_manager.init_app(app)
     db.init_app(app)
     bootstap.init_app(app)
+    migrate.init_app(app,db)
+
+
+
     configure_uploads(app,photos)
     mail.init_app(app)
     
--- manage.py
+++ manage.py
@@ -1,26 +1,25 @@
 from flask_script import Manager, Server
-from flask_migrate import Migrate, MigrateCommand
+from flask_migrate import Migrate,MigrateCommand
 from app import create_app,db
 from app.models import User
 
-app = create_app('production')
+app = create_app('development')
 
 manager = Manager(app)
+manager.add_command('runserver',Server)
 migrate = Migrate(app,db)
-
-manager.add_command('db',MigrateCommand)
-manager.add_command('run',Server(use_debugger=True))
+manager.add_command('db', MigrateCommand)
 
 
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User)
 
-@manager.command
-def test():
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+# @manager.command
+# def test():
+#     import unittest
+#     tests = unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == "__main__":
     manager.run()
